e4668f4527f9f5bb60188324c3d05c908789add8
Michael Santiago <michael.s@knights.ucf.edu>
2012-10-09 17:14:06 -0400

    Generated, Installed Devise, Added Bootstrap.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trex::Application.config.secret_token = '489ae9c2abc8a4d02086ded75abcc1c664776a550c0860244703903385b242eae713720d8166b1ae7796bbbbbb6e22d0ed47cfdeb8d92b656641f18358bba982'
